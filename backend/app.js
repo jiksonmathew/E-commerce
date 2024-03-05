37034cfd99d93455739b6f621b8bb48cf5f39476
Jikson Mathew <jiksonmathew14@gmail.com>
2024-03-05 12:30:55 +0530

    last

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 const express= require("express")
 const app= express()
 const errorMiddleware = require("./middleware/error")
-
+const cookiParser =require("cookie-parser")
 
 app.use(express.json())
+app.use(cookiParser())
 //route import
 
 const product = require("./routes/productRoute")
+const user= require("./routes/userRoute")
 
 app.use("/api/v1",product)
-
+app.use("/api/v1",user)
 //Middleare for errors
 app.use(errorMiddleware)
 
